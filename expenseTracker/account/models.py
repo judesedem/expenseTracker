9d0989d2ff9978b11e95143a43a87c37255df3f1
Jude Fiadzawoo <judefiadzawoo@gmail.com>
2025-11-18 16:23:28 +0000

    Changed dictionaries in categories to tuples

--- expenseTracker/account/models.py
+++ expenseTracker/account/models.py
@@ -3,14 +3,22 @@ from django.db import models
 class User(models.Model):
     first_name=models.CharField(max_length=30)
     last_name=models.CharField(max_length=30)
-    CATEGORY_CHOICES={
-        'G':'General',
-        'F':'Food',
-        'H':'Health',
-        'E':'Education',
-        'B':'Bills'
-    }
-    category=models.CharField(max_length=1,choices=CATEGORY_CHOICES)
+    CATEGORY_CHOICES=[
+        ('G','General'),
+        ('F','Food'),
+        ('H','Health'),
+        ('E','Education'),
+        ('B','Bills')
+    
+    ]
+      
+    category=models.CharField(max_length=1,choices=CATEGORY_CHOICES,default='G')
+    amount=models.DecimalField(max_length=10,decimal_places=2)
     start_date=models.DateField()
     end_date=models.DateField(auto_now=True)
-# Create your models here.
+
+    def __str__(self):
+        return f"{self.first_name} {self.second_name}-{self.get_category()}"
+    
+    
+
