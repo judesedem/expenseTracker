7bcedbf6ad8636bc83266f918d5722bc3fb92e8c
Jude Fiadzawoo <judefiadzawoo@gmail.com>
2025-11-23 04:33:55 +0000

    Added the API views using viewsets

--- expenseTracker/expense/views.py
+++ expenseTracker/expense/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Category,Expense
+from .serializers import CategorySerializer,ExpenseSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset=Category.objects.all()
+    serializer_class=CategorySerializer
+
+class ExpenseViewSet(viewsets.ModelViewSet):
+    queryset=Expense.objects.all
+    serializer_class=ExpenseSerializer
 
 # Create your views here.
